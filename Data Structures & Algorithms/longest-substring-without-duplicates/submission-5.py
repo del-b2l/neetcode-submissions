86795f9a818b7d012ab98c1ea3a916a5476847dc
batool <150889376+del-b2l@users.noreply.github.com>
2026-07-19 16:09:44 +0500

    Add: longest-substring-without-duplicates - submission-5

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-5.py
@@ -0,0 +1,30 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        """
+        uniqueChars = set()
+        res = 0
+        l = 0
+        
+        for r in range(len(s)): # ensures r doesn't go out of bounds
+            while s[r] in uniqueChars:
+                uniqueChars.remove(s[l])
+                l += 1
+            uniqueChars.add(s[r])
+            res = max(res, r - l + 1)
+
+        return res
+        """
+
+        # more optimal sol: hash map
+        # jump the left pointer directly to the correct position
+        uniqueChars = {} # stores last index for each char appeared
+        res = 0
+        l = 0
+
+        for r in range(len(s)):
+            if s[r] in uniqueChars:
+                l = max(uniqueChars[s[r]] + 1, l)
+            uniqueChars[s[r]] = r # last occurrence's index
+            res = max(res, r - l + 1)
+        
+        return res
